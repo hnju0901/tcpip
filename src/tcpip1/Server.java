f15cd7fc6401d571bd8e1e01a2715e906a2ebbca
student <student@M110315>
2018-01-30 13:53:33 +0900

    love

--- src/tcpip1/Server.java
+++ src/tcpip1/Server.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class Server {
 	public static void main(String[] args) {
 
