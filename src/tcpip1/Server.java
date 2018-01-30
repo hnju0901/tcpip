178a4d532aacf07ae99b0633df82a242d50fefba
student <student@M110315>
2018-01-30 13:57:05 +0900

    love

--- src/tcpip1/Server.java
+++ src/tcpip1/Server.java
@@ -9,7 +9,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 public class Server {
 	public static void main(String[] args) {
-
 		ServerSocket serverSocket = null;
 		Socket socket = null;
 		InputStream in = null;
